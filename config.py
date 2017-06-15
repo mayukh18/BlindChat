994047d3929745b0e15dc92cb57626529d0caeaf
mayukh18 <mayukh.superb@gmail.com>
2017-06-15 15:54:23 +0530

    bug in config

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 #basedir = os.path.abspath(os.path.dirname(__file__))
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = os.environ('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 #SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
