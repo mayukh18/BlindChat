8ec81dec7d030f874034b06ed799f2d4e6324374
mayukh18 <mayukh.superb@gmail.com>
2017-06-15 17:41:41 +0530

    maago

--- app.py
+++ app.py
@@ -109,5 +109,4 @@ def webhook():
 
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
