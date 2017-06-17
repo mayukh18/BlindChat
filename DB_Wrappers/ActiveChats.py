bbac0153774bcf59849a8a21cd10dcfbe38ae693
mayukh18 <mayukh.superb@gmail.com>
2017-06-17 17:56:05 +0530

    activechats

--- DB_Wrappers/ActiveChats.py
+++ DB_Wrappers/ActiveChats.py
@@ -18,22 +18,28 @@ class ActiveChatsDB:
     def create_new_chat(self, user1, user2):
         u1 = ActiveChatsUser(id=user1, partner=user2)
         u2 = ActiveChatsUser(id=user2, partner=user1)
-        self.db.session.add(u1)
-        self.db.session.add(u2)
+
+        try:
+            self.db.session.add(u1)
+        except:
+            self.db.session.delete(u1)
+            self.db.session.add(u1)
+
+        try:
+            self.db.session.add(u2)
+        except:
+            self.db.session.delete(u2)
+            self.db.session.add(u2)
+
         self.db.session.commit()
 
     def delete_chat_entries(self, user):
-        print("1")
         partner = self.get_partner(user=user)
         user1 = ActiveChatsUser.query.get(user)
         user2 = ActiveChatsUser.query.get(partner)
-        print("2")
         self.db.session.delete(user1)
-        print("3")
         self.db.session.delete(user2)
-        print("4")
         self.db.session.commit()
-        print("5")
 
     def get_alias(self, user):
         u = ActiveChatsUser.query.get(user)
