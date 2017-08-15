60142d7a0bc2fe1cff3a366c8b2336294d61fb13
mayukh18 <mayukh.superb@gmail.com>
2017-08-15 12:55:07 +0530

    change

--- modules/gifs.py
+++ modules/gifs.py
@@ -1,5 +1,5 @@
 _male_start_hi = ['https://media.giphy.com/media/dzaUX7CAG0Ihi/giphy-downsized.gif',
-                  'https: // media.giphy.com / media / oJiCqvIqPZE3u / giphy.gif']
+                  'https://media.giphy.com/media/oJiCqvIqPZE3u/giphy.gif']
 
 _female_start_hi = ['https://media.giphy.com/media/a1QLZUUtCcgyA/giphy-downsized.gif',
                     'https://media.giphy.com/media/EPJZhOrStSpz2/giphy-downsized.gif']
@@ -7,8 +7,10 @@ _female_start_hi = ['https://media.giphy.com/media/a1QLZUUtCcgyA/giphy-downsized
 import random
 def get_start_hi(gender):
     if gender == "male":
-        return random.choice(_male_start_hi)
+        #return random.choice(_male_start_hi)
+        return _male_start_hi[1]
     elif gender == "female":
-        return random.choice(_female_start_hi)
+        #return random.choice(_female_start_hi)
+        return _female_start_hi[1]
     else:
         return random.choice(_male_start_hi)
