388f8be602eb7e65c78099f6f34c67e160ff1880
mayukh18 <mayukh.superb@gmail.com>
2017-08-26 17:33:27 +0530

    send images in conversations

--- modules/image.py
+++ modules/image.py
@@ -4,6 +4,7 @@ from utilities import send_message
 
 def handle_image(sender, url):
     try:
+        print("IMAGE 2.5")
         partner = activechatsdb.get_partner(sender)
         alias = activechatsdb.get_alias(sender)
         print("IMAGE 3")
@@ -12,5 +13,6 @@ def handle_image(sender, url):
         print("IMAGE 4")
         message = AttachmentTemplate(url=url, type='image')
         send_message(message.get_message(), id=partner)
+        print("IMAGE 4.5")
     except Exception, e:
         print("IMAGE ERROR", str(e))
